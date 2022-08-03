1b56f955a39870ae329b238165fffe20a3620544
unknown <henokmekuanint79@gmail.com>
2022-08-03 14:09:27 +0300

    bootstrapadded

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import DefaultLayout from '../components/DefaultLayout';
 function Home(){
 
     return(
         <div>
+            <DefaultLayout>
             <h1>Home page</h1>
+            </DefaultLayout>
         </div>
     )
 }
